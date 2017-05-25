92acb69d8043ad2d530b1553e7dc8abd81ed60d9
Kynosaur <kye.bracey@gmail.com>
2017-05-25 13:02:28 +0100

    Move responsibility of storing player names from session to global variables

--- app.rb
+++ app.rb
@@ -10,24 +10,24 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     session[:player_1_HP] = 50
     session[:player_2_HP] = 50
     redirect '/play'
     end
 
     get '/play' do
-      @player_1_name = session[:player_1_name]
-      @player_2_name = session[:player_2_name]
+      @player_1_name = $player_1.name
+      @player_2_name = $player_2.name
       @player_1_HP = session[:player_1_HP]
       @player_2_HP = session[:player_2_HP]
       erb :play
     end
 
     get '/attack' do
-      @player_1_name = session[:player_1_name]
-      @player_2_name = session[:player_2_name]
+      @player_1_name = $player_1.name
+      @player_2_name = $player_2.name
       erb :attack
     end
 
