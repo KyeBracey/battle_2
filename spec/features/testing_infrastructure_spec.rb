758024986e001bf3df19de073870d8fecf905c9c
Kynosaur <kye.bracey@gmail.com>
2017-05-25 12:26:35 +0100

    Add ability for player 1 to attack player 2 and receive a confirmation

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -12,3 +12,11 @@
      expect(page).to have_content "Opponent has 50HP!"
    end
  end
+
+ feature "Attack" do
+   scenario "Player 1 attacks player 2 and gets a confirmation" do
+     sign_in_and_play
+     click_button('Attack')
+     expect(page).to have_content "Jennifer attacks Canard!"
+   end
+ end
